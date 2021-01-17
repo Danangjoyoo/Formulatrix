1232df49887f8e6925a64d862b5ca28f4d48abf4
Danangjoyoo <68761014+Danangjoyoo@users.noreply.github.com>
2021-01-17 21:37:56 +0700

    Testing

--- test_ajah/local_updater.py
+++ test_ajah/local_updater.py
@@ -4,7 +4,7 @@ import time, os
 class FloFeature_Database():
     def __init__(self, filename):
         self.filename = 'test_ajah/'+filename
-        self.g = Github('b45bc04c9e645a18e0c409520c8770b8ea836f00')
+        self.g = Github('fd706dff3e16707b2e4e5a82221b1ecc22a64ae7')
         self.all_repos = self.g.get_user().get_repos()
         self.taracuss_repo = None
         for i,repo in enumerate(self.all_repos):
@@ -26,15 +26,16 @@ class FloFeature_Database():
     def readRaw(self):
         return str(self.taracuss_repo.get_contents(self.filename).decoded_content).split("'")[1]
 
-    def readList(self):
-        return [data for data in self.readRaw().split(';')]
-
 
 filelist = os.listdir(os.getcwd())
 filePack = []
 
 for i, file in enumerate(filelist):
+    print("Creating Database...")
     filePack.append(FloFeature_Database(file))
+    time.sleep(5)
+    print("Storing data to the cloud")
     f = open(file, 'r')
     contents = f.read()
     filePack[i].update("Testing", contents)
+    print("Finished", file)

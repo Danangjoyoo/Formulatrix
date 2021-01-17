79bcd032ebeb9b1febd4be28155dc29d9284b8f2
Danangjoyoo <68761014+Danangjoyoo@users.noreply.github.com>
2021-01-17 21:33:02 +0700

    Testing

--- test_ajah/local_updater.py
+++ test_ajah/local_updater.py
@@ -4,7 +4,7 @@ import time, os
 class FloFeature_Database():
     def __init__(self, filename):
         self.filename = 'test_ajah/'+filename
-        self.g = Github('57e2b1980667933e43fd6e1c8c750abd7cb33aa5')
+        self.g = Github('b45bc04c9e645a18e0c409520c8770b8ea836f00')
         self.all_repos = self.g.get_user().get_repos()
         self.taracuss_repo = None
         for i,repo in enumerate(self.all_repos):
@@ -16,12 +16,12 @@ class FloFeature_Database():
 
     def delete(self):
         content = self.taracuss_repo.get_contents(self.filename)
-        self.taracuss_repo.delete_file(content.path, 'remove',content.sha)
+        self.taracuss_repo.delete_file(content.path, 'remove', content.sha)
 
     def update(self, comment, newContent):
         currentContent = self.readRaw() + ';'
         content = self.taracuss_repo.get_contents(self.filename)
-        self.taracuss_repo.update_file(content.path, comment, currentContent+newContent, content.sha)
+        self.taracuss_repo.update_file(content.path, comment, newContent, content.sha)
 
     def readRaw(self):
         return str(self.taracuss_repo.get_contents(self.filename).decoded_content).split("'")[1]
@@ -35,7 +35,6 @@ filePack = []
 
 for i, file in enumerate(filelist):
     filePack.append(FloFeature_Database(file))
-    #filePack[i].delete()
     f = open(file, 'r')
     contents = f.read()
-    filePack[i].create("Testing", contents)
+    filePack[i].update("Testing", contents)

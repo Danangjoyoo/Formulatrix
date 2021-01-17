32855a00b4dc790ad015d3f4bad22c0f9b39f97c
Danangjoyoo <68761014+Danangjoyoo@users.noreply.github.com>
2021-01-17 22:33:56 +0700

    Testing

--- test_ajah/local_updater.py
+++ test_ajah/local_updater.py
@@ -4,7 +4,7 @@ import time, os
 class FloFeature_Database():
     def __init__(self, filename):
         self.filename = 'test_ajah/'+filename
-        self.g = Github('fad5326cebfdcf0039c5d69f0d47b11896b75fb3')
+        self.g = Github('0bed130be30d056b35305d406e399a54d452115d')
         self.all_repos = self.g.get_user().get_repos()
         self.branchName = 'main'
         self.taracuss_repo = None
@@ -30,13 +30,17 @@ class FloFeature_Database():
 filelist = os.listdir(os.getcwd())
 filePack = []
 
-for i, file in enumerate(filelist):
-    print("Creating Database...")
-    filePack.append(FloFeature_Database(file))
-    #filePack[i].delete()
-    print("Storing data to the cloud")
-    f = open(file, 'r')
-    contents = f.read()
-    f.close()
-    filePack[i].create("Testing", contents)
-    print("Finished", file)
+i = 0
+while True:
+    for i, file in enumerate(filelist):
+        print("Creating Database...")
+        filePack.append(FloFeature_Database(file))
+        #filePack[i].delete()
+        print("Storing data to the cloud")
+        f = open(file, 'r')
+        contents = f.read()
+        f.close()
+        filePack[i].update("Testing", contents)
+        print("Finished", file)
+    i += 1
+    print(i)

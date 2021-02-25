6561030f96ef17d3222713bc117b67d996953b32
Danangjoyoo <68761014+Danangjoyoo@users.noreply.github.com>
2021-02-25 17:33:56 +0700

    Testing

--- 2021/schV2/visualize.py
+++ 2021/schV2/visualize.py
@@ -2,22 +2,34 @@ import colorama as cora
 
 # VISUALIZATION
 cora.init()
-def printr(*args):
+def printr(*args,**kargs):
     text = ''
     for t,i in enumerate(args): text += str(i)+' ' if t+1 <= len(args) else str(i)
-    print(cora.Fore.RED+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)
+    if kargs:
+        print(cora.Fore.RED+cora.Style.BRIGHT+text+cora.Style.RESET_ALL,**kargs)
+    else:
+        print(cora.Fore.RED+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)
 
-def printb(*args):
+def printb(*args,**kargs):
     text = ''
     for t,i in enumerate(args): text += str(i)+' ' if t+1 <= len(args) else str(i)
-    print(cora.Fore.CYAN+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)
+    if kargs:
+        print(cora.Fore.CYAN+cora.Style.BRIGHT+text+cora.Style.RESET_ALL,**kargs)
+    else:
+        print(cora.Fore.CYAN+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)
 
-def printg(*args):
+def printg(*args,**kargs):
     text = ''
     for t,i in enumerate(args): text += str(i)+' ' if t+1 <= len(args) else str(i)
-    print(cora.Fore.GREEN+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)
+    if kargs:
+        print(cora.Fore.GREEN+cora.Style.BRIGHT+text+cora.Style.RESET_ALL,**kargs)
+    else:
+        print(cora.Fore.GREEN+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)
 
-def printy(*args):
+def printy(*args,**kargs):
     text = ''
     for t,i in enumerate(args): text += str(i)+' ' if t+1 <= len(args) else str(i)
-    print(cora.Fore.YELLOW+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)
+    if kargs:
+        print(cora.Fore.YELLOW+cora.Style.BRIGHT+text+cora.Style.RESET_ALL,**kargs)
+    else:
+        print(cora.Fore.YELLOW+cora.Style.BRIGHT+text+cora.Style.RESET_ALL)

f5e8cd6ff58796160f5db07a450e62719ab7b6cb
Danangjoyoo <68761014+Danangjoyoo@users.noreply.github.com>
2021-03-22 12:33:11 +0700

    Testing

--- 2021/schV2/misc.py
+++ 2021/schV2/misc.py
@@ -36,37 +36,16 @@ import pandas as pd
 
 !!!!!!!!!! README !!!!!!!!!!
 - You can stream sensors reading by just using liveplot()
-- If you want to execute a function while liveplotting, use run() but with caution:
-    - These plotters can't be executed in subthread, only able to be executed in main thread
-    - So your executed function will be executed in the subthread.
-    - Make sure your executed function is able to execute in subthread instead of main thread.
-    - Make sure you have safety break in your function (Highly recommended for function with iterations (loop))
-    - To increase the safety, there is a function to stop liveplotting --> terminate()
-- This plotter might only works with FLO v2 single channel, your object should be pointing to firmware call (p or c.p)
+- This plotter might only works with FLO v2 single channel
+- Your object should be pointing/referencing to v2 channel firmware call (p or c.p)
 - You have to override autoUpdate() method For any plotting outside the defaults.
 
-Plotter --> Matplotlib Plotter (Python Build)
-+ Saver use
-+ No disturb other event hooks
-- Low Speed (70-300 ms/tick)
-
-CPlotter --> PyQt Plotter (C/C++ Wrapper)
-+ High Speed (10 - 70 ms/tick)
-- Usually Disturbs other event hooks (pyreadline, etc)
-- This disturbance make some function of other event hooks is disabled (Keyboard Interrupt)
-- To recover from this error, just manually exit your program and start your script again.
-
-SPlotter --> Safe PyQt Plotter (Manual Multiprocessing)
-+ High Speed (5-30 ms/tick)
-+ Run in different GIL (multiprocess)
-- Manual SetUp and reset
 """
-
-#from schPlotter2 import *
 from SPlotter import *
 
 
 ######################### PID TUNER #########################
+# under development...
 class Tuner():
 	def __init__(self):
 		self.execFunc = None
